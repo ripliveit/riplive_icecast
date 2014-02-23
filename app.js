9d3cb14b9d5d65dc11621904c7fbfd5110c55bee
gabrieledarrigo <darrigo.g@gmail.com>
2014-02-23 14:11:11 +0100

    Only a single radio stream socket is used to retrieve Shoutcast info. Introduce logging with bunyan. Various bug fixing

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 var engine = require('engine.io');
-var radio  = require('radio-stream');
+var radio = require('radio-stream');
 var broadway = require('broadway');
-var request  = require('request');
+var request = require('request');
 var querystring = require('querystring');
+var bunyan = require('bunyan');
 
 var app = new broadway.App();
 
 /**
  * Load and inject all application modules and dependecies.
  */
-app.use(require(__dirname + '/lib/server.js'), { 
-	engine : engine
+app.use(require(__dirname + '/lib/logger.js'), {
+    bunyan: bunyan
+});
+
+app.use(require(__dirname + '/lib/server.js'), {
+    logger: app.getLogger(),
+    engine: engine
 });
 
 app.use(require(__dirname + '/lib/parser.js'), {
-	request : request,
-	querystring : querystring
+    logger: app.getLogger(),
+    request: request,
+    querystring: querystring
 });
 
-app.use(require(__dirname + '/lib/radio.js'), { 
-	radio : radio,
-	parser : app.getParser()
+app.use(require(__dirname + '/lib/radio.js'), {
+    logger: app.getLogger(),
+    radio: radio,
+    parser: app.getParser()
 });
 
-
-app.init(function (err) {
-  	if (err) {
-    	console.log(err);
-  	}
+app.init(function(err) {
+    if (err) {
+        console.log(err);
+    }
 });
 
 app.on('error', function(err) {
-	console.log(err);
+    console.log(err);
 });
 
 /**
